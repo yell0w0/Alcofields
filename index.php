6127df352909984b0b72b3890aec2501cf901326
mejakep <mejakep@users.noreply.github.com>
2021-12-01 22:02:05 +0000

    Co-authored-by: yell0w0 <yell0w0@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            FUCK   
+        </title>
+    </head>
+    <body>
+        <h1>
+            big balls website
+        </h1>
+        something here like <em>italic</em> text.
+        
+    </body>
+</html>
